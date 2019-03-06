d30026bb4725b0c42d710dee85158f9c1db01eec
angeliquecd <acharles-davis00@stuy.edu>
2019-03-06 10:15:34 -0500

    finished reading in directions

--- USACO.java
+++ USACO.java
@@ -10,10 +10,13 @@ public class USACO{
     int index= 0;
     int[] directions = new int[4];
     for (int j =0;j<firstline.length();j++){
-      if (firstline.charAt(j)==' '|| j==firstline.length()-1) {
+      if (firstline.charAt(j)==' ') {
         directions[index]= Integer.parseInt(firstline.substring(old,j));
       index++;
       old=j+1;}
+      if (j==firstline.length()-1){
+        directions[3]= Integer.parseInt(firstline.substring(old,j+1));
+      }
     }
     System.out.println(directions[0]+" "+directions[1]+" "+directions[2]+" "+directions[3]);
     System.out.println(firstline);
