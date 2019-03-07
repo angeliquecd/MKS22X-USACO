2825d65ae5a7da88729f8de78f6efbf974cd0211
angeliquecd <acharles-davis00@stuy.edu>
2019-03-07 10:12:06 -0500

    found a much simpler way to read in file and used that

--- USACO.java
+++ USACO.java
@@ -9,7 +9,7 @@ public class USACO{
     int old = 0;
     int index= 0;
     int[] directions = new int[4];
-    for (int j =0;j<firstline.length();j++){
+    /*for (int j =0;j<firstline.length();j++){
       if (firstline.charAt(j)==' ') {
         directions[index]= Integer.parseInt(firstline.substring(old,j));
       index++;
@@ -17,17 +17,55 @@ public class USACO{
       if (j==firstline.length()-1){
         directions[3]= Integer.parseInt(firstline.substring(old,j+1));
       }
-    }
-    System.out.println(directions[0]+" "+directions[1]+" "+directions[2]+" "+directions[3]);
-    System.out.println(firstline);
-    int[][] field = new int[directions[0]][directions[1]];
-    while (inf.hasNextLine()){
+    }*/
+  //  System.out.println(directions[0]+" "+directions[1]+" "+directions[2]+" "+directions[3]);
+    //System.out.println(firstline);
+    String[] directionline = firstline.split(" ");
+    int[][] field = new int[Integer.parseInt(directionline[0])][Integer.parseInt(directionline[1])];
+    index=-1;
+    int row=0;
+     int newly =0;
+    while (index<field.length-1){
+      index++;
+      newly=0;
+      row=0;
       String line = inf.nextLine();
-      //for (int i =0;i<line.length;i++){
-        //if (line.charAt(i)=='
-        //}
-      System.out.println(line);
+      for (int i=0;i<line.length();i++){
+        if (line.charAt(i)==' '){
+        //  System.out.println("newly: "+newly+"i: "+i);
+          field[index][row]=Integer.parseInt(line.substring(newly,i));
+          row++;
+          newly=i+1;}
+        if (i==line.length()-1) field[index][directions[1]-1]= Integer.parseInt(line.substring(newly,i+1));
       }
+    //  System.out.println(line);
+      }
+    /*  for (int a=0;a<field.length;a++){
+        System.out.println("\n");
+        for (int b=0;b<field[0].length;b++){
+          System.out.println(field[a][b]);
+        }*/
+        int direction=1;
+      while(inf.hasNextLine()){
+        String line=inf.nextLine();
+        System.out.println(line);
+        String[] direction2= line.split(" ");
+         row = Integer.parseInt(direction2[0]);
+        int col= Integer.parseInt(direction2[1]);
+        int stomp=Integer.parseInt(direction2[2]);
+        int lowest = 0;
+        int highestrow;
+        int highestcol;
+        for (int inc=0;inc<=2;inc++){
+        for (int i=0;i<=2;i++){
+          if (row+inc<field.length && col+i<field[0].length){
+          if (field[row+inc][col+i]>highest) {
+            field[row+inc][col+i]=highest;
+            highestrow=row+inc;
+            highestcol=col+i;
+        }}
+      }}
+      field[highestrow][highestcol]-=stomp;
     return 10;}
     catch (FileNotFoundException e){
       e.printStackTrace();
