664b98d160e1a1a1e9572bf2f08ad1f742a159e5
angeliquecd <acharles-davis00@stuy.edu>
2019-03-10 14:30:16 -0400

    got it to work with one puzzle- forgot to commit most of other work

--- USACO.java
+++ USACO.java
@@ -22,64 +22,83 @@ public class USACO{
     //System.out.println(firstline);
     String[] directionline = firstline.split(" ");
     int[][] field = new int[Integer.parseInt(directionline[0])][Integer.parseInt(directionline[1])];
+    printpuzzle(field);
     index=-1;
     int row=0;
      int newly =0;
     while (index<field.length-1){
       index++;
-      newly=0;
-      row=0;
       String line = inf.nextLine();
-      for (int i=0;i<line.length();i++){
-        if (line.charAt(i)==' '){
-        //  System.out.println("newly: "+newly+"i: "+i);
-          field[index][row]=Integer.parseInt(line.substring(newly,i));
-          row++;
-          newly=i+1;}
-        if (i==line.length()-1) field[index][directions[1]-1]= Integer.parseInt(line.substring(newly,i+1));
+    //  System.out.println(line);
+      String[] lines = line.split(" ");
+      for (int i=0;i<field[0].length;i++){
+      //  System.out.println(Integer.parseInt(lines[i]));
+        field[index][i]=Integer.parseInt(lines[i]);
+        //printpuzzle(field);
       }
     //  System.out.println(line);
       }
-    /*  for (int a=0;a<field.length;a++){
-        System.out.println("\n");
-        for (int b=0;b<field[0].length;b++){
-          System.out.println(field[a][b]);
-        }*/
-        int direction=1;
+      System.out.println(printpuzzle(field));
+
       while(inf.hasNextLine()){
         String line=inf.nextLine();
         System.out.println(line);
         String[] direction2= line.split(" ");
-         row = Integer.parseInt(direction2[0]);
-        int col= Integer.parseInt(direction2[1]);
+         row = Integer.parseInt(direction2[0])-1;
+        int col= Integer.parseInt(direction2[1])-1;
         int stomp=Integer.parseInt(direction2[2]);
+        System.out.println(""+row+" "+col+" "+stomp);
         int highest = 0;
         int highestrow=0;
         int highestcol=0;
-        boolean found=true;
-        while(found){
-          found=false;
         for (int inc=0;inc<=2;inc++){
         for (int i=0;i<=2;i++){
           if (row+inc<field.length && col+i<field[0].length){
           if (field[row+inc][col+i]>highest) {
-            field[row+inc][col+i]=highest;
+            highest=field[row+inc][col+i];
             highestrow=row+inc;
             highestcol=col+i;
-            found=true;
-        }}
-      }}
-      field[highestrow][highestcol]-=stomp;}
-        for (int a=0;a<field.length;a++){
-          System.out.println("\n");
-          for (int b=0;b<field[0].length;b++){
-            System.out.print(field[a][b]+" ");
-          }
-    return 10;}}
+            }
+        }
+      }
+    }
+      field[highestrow][highestcol]-=stomp;
+      for (int inc=0;inc<=2;inc++){
+      for (int i=0;i<=2;i++){
+        if (row+inc<field.length && col+i<field[0].length){
+        if (field[row+inc][col+i]>field[highestrow][highestcol]){
+          field[row+inc][col+i]=field[highestrow][highestcol];}
+      }
+    }
+    }
+      System.out.println(printpuzzle(field));
+    }//end of directions
+  int elevation = Integer.parseInt(directionline[2]);
+  int depth=0;
+  for (int a=0;a< field.length;a++){
+    for (int b=0;b<field[0].length;b++){
+      if (field[a][b]<elevation){
+        depth+=elevation-field[a][b];
+      }
+    }
+  }
+  System.out.println(depth*72*72);
+    return depth*72*72;
+  }//end of try
     catch (FileNotFoundException e){
       e.printStackTrace();
     }
     return 3;
+}
+public static String printpuzzle(int[][] puzzle){
+  String value="";
+  for (int a=0;a< puzzle.length;a++){
+    for (int b=0;b<puzzle[0].length;b++){
+      value+=" "+puzzle[a][b];
+    }
+    value+="\n";
+  }
+  return value;
 }
   public static int silver(String filename) throws FileNotFoundException{
 return 4;
